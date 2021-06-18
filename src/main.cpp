d02e8c76e81fc7814a222aaf700d31199163b248
David Arthur <mumrah@gmail.com>
2021-06-18 11:35:05 -0400

    Use interrupt for Nino TX as well

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,11 @@
+#define FOSC 7372800L // Clock Speed
+#define NINO_BAUD 57600
+#define NINO_UBRR FOSC/16/NINO_BAUD-1
+
 #include <Arduino.h>
 #include <SoftwareSerial.h>
 #include "BM70.h"
+#include <avr/interrupt.h>
 
 SoftwareSerial DebugSerial(PD6, PD7);  
 
@@ -13,6 +18,15 @@ const uint8_t status[43] = {
   0x73, 0x20, 0x77, 0x6f, 0x72, 0x6b, 0x69, 0x6e, 0x67, 0x21, 0xc0
 };
 const uint8_t statusLen = 43;
+
+unsigned long last_tick = 0L;
+unsigned long last_nino_read = 0L;
+uint8_t nino_read_pos = 0;
+uint8_t nino_packet[400]; 
+
+uint8_t nino_tx_packet[200]; 
+uint8_t nino_tx_pos = 0;
+uint8_t nino_tx_len = 0;
 
 /**
  * Pretty print a byte array as hex characters
@@ -31,20 +45,43 @@ void printBytes(uint8_t * bytes, size_t len)
   DebugSerial.println();
 }
 
+void USART1_Init(unsigned int ubrr)
+{
+    UBRR1L = (unsigned char)ubrr; 
+    UCSR1B = (1<<RXEN1) | (1<<TXEN1) | (1<<RXCIE1);  
+    UCSR1C =  (3<<UCSZ00);
+}
+
 void ble_callback(uint8_t * buffer, uint8_t len)
 {
   if (buffer[0] == 0xC0 && buffer[len-1] == 0xC0)
   {
     DebugSerial.print("Writing "); DebugSerial.print(len); DebugSerial.println(" bytes to NinoTNC");
-    Serial1.write(buffer, len);
-    Serial1.flush();
+    for (uint8_t i = 0; i<len; i++)
+      nino_tx_packet[i] = buffer[i];
+    nino_tx_len = len;
+    nino_tx_pos = 0;
+    UCSR1B |= (1<<UDRIE1);
+    DebugSerial.println("send");
   }
   else
   {
     DebugSerial.print("Skipping non-KISS data");
     printBytes(buffer, len);
-  }
-  
+  } 
+}
+
+ISR(USART1_RX_vect)
+{
+  nino_packet[nino_read_pos++] = UDR1;
+}
+
+ISR(USART1_UDRE_vect)
+{ 
+  if (nino_tx_pos < nino_tx_len)
+    UDR1 = nino_tx_packet[nino_tx_pos++];
+  else
+    UCSR1B &= ~(1 << UDRIE1);
 }
 
 void setup()
@@ -61,21 +98,17 @@ void setup()
   digitalWrite(PD5, LOW);
 
   DebugSerial.begin(19200);
-  DebugSerial.println("NinoTNC + BLE Setup");
+  DebugSerial.print("\n~~ NinoTNC + BLE by K4DBZ ~~\n");
 
   bm70 = BM70(&Serial, 19200, ble_callback);
   bm70.reset();
 
-  Serial1.begin(57600);
   delay(3000);
   
-  Serial1.write(status, statusLen);
+  USART1_Init(NINO_UBRR);
+  sei();
+  //USART1_Transmit(status, statusLen);
 }
-
-unsigned long last_tick = 0L;
-unsigned long last_nino_read = 0L;
-uint8_t nino_read_pos = 0;
-uint8_t nino_packet[400]; 
 
 void loop()
 {
@@ -91,22 +124,21 @@ void loop()
 
   bm70.read();
 
-  // NinoTNC
-  if (Serial1.available() > 0) 
+  // Check if we read a data from NinoTNC
+  if ((now - last_nino_read) > 200)
   {
-    uint8_t b = Serial1.read();
-    DebugSerial.print("NinoTNC "); DebugSerial.print(nino_read_pos);
-    DebugSerial.print(" "); DebugSerial.print(b, HEX); DebugSerial.print(" "); DebugSerial.println(char(b));
-    nino_packet[nino_read_pos++] = b;
+    if (nino_read_pos > 0 && nino_packet[nino_read_pos-1] == 0xC0) 
+    {
+      printBytes(nino_packet, nino_read_pos);
+      //bm70.send(status, statusLen);
+      nino_read_pos = 0;
+    }
+    else if ((now - last_nino_read) > 1000)
+    {
+      DebugSerial.println("Timed out reading KISS from NinoTNC");
+      nino_read_pos = 0;
+    }
     last_nino_read = now;
-    return;
-  }
-
-  if (nino_read_pos > 0 && (now - last_nino_read) > 200)
-  {
-    DebugSerial.println("Writing to BM70");
-    bm70.send(status, statusLen);
-    nino_read_pos = 0;
   }
 
   if (bm70.updateStatus())
